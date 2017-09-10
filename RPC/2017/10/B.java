bda3c49000a29531bc91b3fbc558def7ea5d2991
Eric Loza Montaño <lozaeric@gmail.com>
2017-09-10 12:24:22 -0300

    delete unused lines

--- RPC/2017/10/B.java
+++ RPC/2017/10/B.java
@@ -4,10 +4,8 @@ import java.util.Scanner;
 
 
 class B {
-	static int arr[];
     public static void main(String[] args) {
        Scanner in = new Scanner(System.in);
-       arr = new int[]{4,2,3,5,1,0};
        int t = in.nextInt();
        while (t-- > 0) {
     	   int n = in.nextInt();
