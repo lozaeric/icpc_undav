33482957b6d403bda6c24db18bcf73a5620c0e6e
lozaeric <lozaeric@gmail.com>
2017-09-23 00:34:44 -0300

    bsearch

--- Implementaciones/BTSearch.java
+++ Implementaciones/BTSearch.java
@@ -1,4 +1,4 @@
-class TernarySearch {
+class BTSearch {
 	double epsilon = 0.000001d;
 	
 	// Ternary Search
@@ -23,4 +23,20 @@ class TernarySearch {
 				right = rt;
 		}
 	}
+	
+   int binarySearch(int arr[], int x) {
+       int l = 0, r = arr.length - 1;
+       while (l <= r) {
+           int m = l+(r-l)/2;
+
+           if (arr[m] == x)
+               return m;
+
+           if (arr[m] < x)
+               l = m+1;
+           else
+               r = m-1;
+       }
+       return -1;
+   }
 }
